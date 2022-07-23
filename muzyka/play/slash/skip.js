0eb7de43424eda5dd4f7123e2e0c744b9dabfe63
Gotowka <89851069+Gotowka@users.noreply.github.com>
2022-07-24 01:39:08 +0200

    Dodano skip

---
+++ muzyka/play/slash/skip.js
@@ -0,0 +1,34 @@
+{
+  "name": "skip",
+  "permissions": "NONE",
+  "permissions2": "NONE",
+  "restriction": "1",
+  "_id": "HLBEB",
+  "actions": [
+    {
+      "": null,
+      "name": "Skip"
+    },
+    {
+      "channel": "0",
+      "varName": "",
+      "message": "Pomyślnie ominięto **${tempVars(\"name\")}**",
+      "buttons": [],
+      "selectMenus": [],
+      "attachments": [],
+      "embeds": [],
+      "reply": true,
+      "ephemeral": false,
+      "tts": false,
+      "overwrite": false,
+      "dontSend": false,
+      "editMessage": "0",
+      "editMessageVarName": "",
+      "storage": "0",
+      "varName2": "",
+      "name": "Send Message"
+    }
+  ],
+  "comType": "4",
+  "description": "Omin aktualną piosenke!"
+}
